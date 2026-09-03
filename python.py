afadc3e334e2fa38a5cc6f43a0d10ad7633ed18d
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-09-03 16:29:42 +0600

    add some loop

--- python.py
+++ python.py
@@ -1,6 +1,9 @@
-#loop
-fuel_rate = 100
-for i in range(10):
-    fuel_rate -= 10
-    print(f"Fuel rate: {fuel_rate}")
-    
+# #loop
+# fuel_rate = 100
+# for i in range(10):
+#     fuel_rate -= 10
+#     print(f"Fuel rate: {fuel_rate}")
+
+distance_from_starting_point = 5000
+fuel_needed = 0.05 * distance_from_starting_point
+print(f"Fuel needed: {fuel_needed}")
